acc6c35db90e3e06f2b12b5cfe94ebb50cea5752
gustavogk <gust.krv@gmail.com>
2019-08-29 19:19:58 -0300

    ULA - FINALIZADA E TESTADA

--- ULA.vhd
+++ ULA.vhd
@@ -8,21 +8,29 @@ port
 (
 ULA_Ain : in std_logic_vector(15 downto 0 );
 ULA_Bin : in std_logic_vector(15 downto 0 );
-ULA_Cout : out std_logic_vector(15 downto 0 );
+ULA_Cout : buffer std_logic_vector(15 downto 0 );
 Sel : in std_logic_vector(1 downto 0);
-Z : out bit;
-n : out bit
+Z : out std_logic;
+n : out std_logic
 );
+
+end entity ULA;
 
 architecture behavior of ULA is
 begin
 
-ULA_Cout <= ULA_Ain + ULA_Bin when (Sel = "00") else
-            ULA_Ain and UlA_bin when (Sel = "01")else
-            ULA_Ain when (Sel = "10")else
-            not ULA_Ain when (Sel = "11"); 
+with Sel select
+ULA_Cout <= ULA_Ain + ULA_Bin when "00",
+            ULA_Ain and ULA_Bin when "01",
+            ULA_Ain when "10",
+            not ULA_Ain when others;
 				
-				if(ULA_Cout(15)='1') then n <= '1'else n <= '0'
-				if(ULA_Cout = "0000000000000000" then z <= '1'else z <= '0'
-				
+with ULA_Cout select
+z <= '1' when "0000000000000000",
+     '0' when others;
+	  
+with ULA_Cout(15) select
+n <= '1' when '1',
+     '0' when others;
+
 end behavior;
